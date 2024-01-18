9e35300d9a50b8e0675e6719a7ea91f14011a0ca
Ricardo Pacheco <rfreitas@live.com>
2024-01-18 10:46:44 -0300

    mudança main

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/designsystem/";
+      config.base = "/design-system/";
     }
 
     return config;
